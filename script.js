bbd08235f35615f0259314c7d76d1802e2a722a9
Pratik <pendurkarpratik04@gmail.com>
2023-05-20 12:20:58 +0530

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 'use strict';
 
-const profleExpander = document.querySelector('.profile');
+const profileExpander = document.querySelector('.profile');
 const mainProfile = document.querySelector('.main');
 let isVisible = false;
 
-profleExpander.addEventListener('click', () => {
+profileExpander.addEventListener('click', () => {
   if (!isVisible) {
     isVisible = true;
     mainProfile.style.display = 'block';
+    mainProfile.style.opacity = '0';
+    profileExpander.style.opacity = '0';
+    profileExpander.style.transform = 'scale(0)';
+    profileExpander.style.backgroundImage =
+      'url("https://api.iconify.design/material-symbols/close.svg")';
+
     setTimeout(() => {
       mainProfile.style.opacity = '1';
-    }, 10); // Delay of 10 milliseconds before changing the opacity
+      profileExpander.style.opacity = '1';
+      profileExpander.style.transform = 'scale(1)';
+    }, 250); // Delay of 10 milliseconds before changing the opacity
   } else {
     mainProfile.style.opacity = '0';
+    profileExpander.style.opacity = '0';
+    profileExpander.style.transform = 'scale(0)';
+    profileExpander.style.backgroundImage = 'url("demo.png")';
+
     setTimeout(() => {
       mainProfile.style.display = 'none';
-    }, 500); // Wait for the animation to complete before hiding the element
+      mainProfile.style.opacity = '1';
+      profileExpander.style.opacity = '1';
+      profileExpander.style.transform = 'scale(1)';
+    }, 250); // Wait for the animation to complete before hiding the element
+
     isVisible = false;
   }
 });
